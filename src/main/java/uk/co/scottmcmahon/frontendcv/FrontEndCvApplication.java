05d74e241c52b2f4d907a2793aea16c8ce9f8589
scott <40343450@live.napier.ac.uk>
2022-08-21 17:23:15 +0100

    init commit

---
+++ src/main/java/uk/co/scottmcmahon/frontendcv/FrontEndCvApplication.java
@@ -0,0 +1,14 @@
+package uk.co.scottmcmahon.frontendcv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontEndCvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontEndCvApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/uk/co/scottmcmahon/frontendcv/FrontEndCvApplicationTest.java
@@ -0,0 +1,13 @@
+package uk.co.scottmcmahon.frontendcv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontEndCvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
